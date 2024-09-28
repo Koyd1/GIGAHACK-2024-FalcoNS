b49aaa91cc1d3d2c5e31976ccbe055190b753108
Alexandr Moroz <log.koyd@gmail.com>
2024-09-28 21:24:56 +0300

    clear

---
+++ src/app/utils/features.js
@@ -0,0 +1,7 @@
+// export const features = [
+//   { text: "Lorem ipsum dolor sit", included: true },
+//   { text: "Cubitur sollicitudin fentum", included: true },
+//   { text: "Nullam interdum enim", included: false },
+//   { text: "Donec ultricies metus", included: false },
+//   { text: "Pellentesque eget nibh", included: false },
+// ];
