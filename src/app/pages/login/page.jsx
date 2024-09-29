cbe8a511b58583dd5ec80ed2bff43f47d2206f23
Alexandr Moroz <log.koyd@gmail.com>
2024-09-29 08:41:55 +0300

    final-ochka

--- src/app/pages/login/page.jsx
+++ src/app/pages/login/page.jsx
@@ -1,68 +1,64 @@
-"use client"
-import React from 'react';
+"use client";
+import React from "react";
 import { redirect } from "next/navigation";
 import { firebase } from "../../utils/firebaseConfig";
 import { collection, getDocs, query, where } from "@firebase/firestore";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 import Link from "next/link";
 
 async function checkUser(formData) {
-    const id = formData.get("id");
-    const password = formData.get("password");
-    const ref = collection(firebase, "users");
-    const querried = [];
-    const q = query(
-        ref,
-        where("idPatient", "==", id),
-        where("password", "==", password),
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        querried.push(doc.data());
-    });
+  const id = formData.get("id");
+  const password = formData.get("password");
+  const ref = collection(firebase, "users");
+  const querried = [];
+  const q = query(
+    ref,
+    where("idPatient", "==", id),
+    where("password", "==", password),
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    querried.push(doc.data());
+  });
 
-    if (querried.length === 0) {
-        Cookies.set("logged_in", "", {path: "/pages"});
-        // document.cookie = "logged_in=";
-        alert("User does not exist!");
-        console.log("Account does not exist!");
-    } else {
-        // document.cookie = "logged_in=true";
-        Cookies.set("logged_in", "true", {path: "/pages"});
-        redirect("/");
-    }
+  if (querried.length === 0) {
+    Cookies.set("logged_in", "", { path: "/pages" });
+    // document.cookie = "logged_in=";
+    alert("User does not exist!");
+    console.log("Account does not exist!");
+  } else {
+    // document.cookie = "logged_in=true";
+    Cookies.set("logged_in", "true", { path: "/pages" });
+    redirect("/");
+  }
 }
-
 
 export default function LoginPage() {
-    // const idRef = useRef();
-    // const passRef = useRef();
+  // const idRef = useRef();
+  // const passRef = useRef();
 
+  // if (getCookie("logged_in")) {
+  if (Cookies.get("logged_in", { path: "/pages" })) {
+    redirect("/");
+  }
 
-    // if (getCookie("logged_in")) {
-    if(Cookies.get("logged_in", {path: "/pages"})) {
-        redirect("/");
-    }
-
-    return (
-        <div>
-            <h2 className="text-2xl ">Login</h2>
-            <form autoComplete="off" action={checkUser}>
-                <p>ID</p>
-                <input name="id" type="text" required/>
-                <p>Password</p>
-                <input name="password" type="password" required/>
-                <button>Login</button>
-                <h3>
-                    Do not have an account:
-                    <Link href="/pages/register">Register Here</Link>
-                </h3>
-            </form>
-
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-2xl text-center mt-20">Login</h2>
+      <form autoComplete="off" action={checkUser}>
+        <p>ID</p>
+        <input name="id" type="text" required />
+        <p>Password</p>
+        <input name="password" type="password" required />
+        <button>Login</button>
+        <h3>
+          Do not have an account:
+          <Link href="/pages/register"> Register Here</Link>
+        </h3>
+      </form>
+    </div>
+  );
 }
-
 
 // export function getCookie(key) {
 //     let b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
--- src/app/pages/register/page.jsx
+++ src/app/pages/register/page.jsx
@@ -1,75 +1,75 @@
 "use client";
 import React, { useRef } from "react";
-import {useRouter} from "next/navigation"
+import { useRouter } from "next/navigation";
 // import { redirect } from "next/navigation";
 import { firebase } from "../../utils/firebaseConfig";
 import { addDoc, collection } from "@firebase/firestore";
 import Link from "next/link";
 
 export default function RegisterPage() {
-    const router = useRouter();
-    // const [data, setData] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
+  const router = useRouter();
+  // const [data, setData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
 
-    const firstNameRef = useRef();
-    const lastNameRef = useRef();
-    const patronRef = useRef();
-    const phoneRef = useRef();
-    const birthDateRef = useRef();
-    const idRef = useRef();
-    const passRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const patronRef = useRef();
+  const phoneRef = useRef();
+  const birthDateRef = useRef();
+  const idRef = useRef();
+  const passRef = useRef();
 
-    const ref = collection(firebase, "users");
+  const ref = collection(firebase, "users");
 
-    const createUser = async (e) => {
-        e.preventDefault();
+  const createUser = async (e) => {
+    e.preventDefault();
 
-        let data = {
-            firstName: firstNameRef.current.value,
-            lastName: lastNameRef.current.value,
-            patronymic: patronRef.current.value,
-            phoneNumber: phoneRef.current.value,
-            dateOfBirth: birthDateRef.current.value,
-            idPatient: idRef.current.value,
-            password: passRef.current.value,
-        };
-
-        try {
-            await addDoc(ref, data);
-            router.push("/pages/login");
-        } catch (error) {
-            console.log(error);
-        }
+    let data = {
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      patronymic: patronRef.current.value,
+      phoneNumber: phoneRef.current.value,
+      dateOfBirth: birthDateRef.current.value,
+      idPatient: idRef.current.value,
+      password: passRef.current.value,
     };
 
-    return (
-        <div>
-            <h1>Register</h1>
-            <div>
-                <form autoComplete="off" onSubmit={createUser}>
-                    <p>First Name</p>
-                    <input name="firstName" type="text" ref={firstNameRef} required/>
-                    <p>Last Name</p>
-                    <input name="lastName" type="text" ref={lastNameRef} required/>
-                    <p>Patronymic</p>
-                    <input name="patron" type="text" ref={patronRef} required/>
-                    <p>Phone Number</p>
-                    <input name="phone" type="text" ref={phoneRef} required/>
-                    <p>Date of Birth</p>
-                    <input name="birthDate" type="text" ref={birthDateRef} required/>
-                    <p>ID</p>
-                    <input name="id" type="text" ref={idRef} required/>
-                    <p>Password</p>
-                    <input name="password" type="password" ref={passRef} required/>
-                    <br/>
-                    <button type="submit">Register</button>
-                    <h3>
-                        Have an account:
-                        <Link href="/pages/login">Sign In</Link>
-                    </h3>
-                </form>
-            </div>
-        </div>
-    );
+    try {
+      await addDoc(ref, data);
+      router.push("/pages/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl text-center mt-20">Register</h1>
+      <div>
+        <form autoComplete="off" onSubmit={createUser}>
+          <p>First Name</p>
+          <input name="firstName" type="text" ref={firstNameRef} required />
+          <p>Last Name</p>
+          <input name="lastName" type="text" ref={lastNameRef} required />
+          <p>Patronymic</p>
+          <input name="patron" type="text" ref={patronRef} required />
+          <p>Phone Number</p>
+          <input name="phone" type="text" ref={phoneRef} required />
+          <p>Date of Birth</p>
+          <input name="birthDate" type="text" ref={birthDateRef} required />
+          <p>ID</p>
+          <input name="id" type="text" ref={idRef} required />
+          <p>Password</p>
+          <input name="password" type="password" ref={passRef} required />
+          <br />
+          <button type="submit">Register</button>
+          <h3>
+            Have an account:
+            <Link href="/pages/login"> Sign In</Link>
+          </h3>
+        </form>
+      </div>
+    </div>
+  );
 }
