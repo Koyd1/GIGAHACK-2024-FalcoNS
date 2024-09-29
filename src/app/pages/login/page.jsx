94a6c96291b20c3e6104c0cc6640260a7854d30b
Really_true_dude <andrekor2004@gmail.com>
2024-09-29 07:45:12 +0300

    log-reg

--- src/app/pages/login/page.jsx
+++ src/app/pages/login/page.jsx
@@ -6,6 +6,64 @@ import { collection, getDocs, query, where } from "@firebase/firestore";
 import Cookies from 'js-cookie';
 import Link from "next/link";
 
+async function checkUser(formData) {
+    const id = formData.get("id");
+    const password = formData.get("password");
+    const ref = collection(firebase, "users");
+    const querried = [];
+    const q = query(
+        ref,
+        where("idPatient", "==", id),
+        where("password", "==", password),
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        querried.push(doc.data());
+    });
+
+    if (querried.length === 0) {
+        Cookies.set("logged_in", "", {path: "/pages"});
+        // document.cookie = "logged_in=";
+        alert("User does not exist!");
+        console.log("Account does not exist!");
+    } else {
+        // document.cookie = "logged_in=true";
+        Cookies.set("logged_in", "true", {path: "/pages"});
+        redirect("/");
+    }
+}
+
+
+export default function LoginPage() {
+    // const idRef = useRef();
+    // const passRef = useRef();
+
+
+    // if (getCookie("logged_in")) {
+    if(Cookies.get("logged_in", {path: "/pages"})) {
+        redirect("/");
+    }
+
+    return (
+        <div>
+            <h2 className="text-2xl ">Login</h2>
+            <form autoComplete="off" action={checkUser}>
+                <p>ID</p>
+                <input name="id" type="text" required/>
+                <p>Password</p>
+                <input name="password" type="password" required/>
+                <button>Login</button>
+                <h3>
+                    Do not have an account:
+                    <Link href="/pages/register">Register Here</Link>
+                </h3>
+            </form>
+
+        </div>
+    );
+}
+
+
 // export function getCookie(key) {
 //     let b = document.cookie.match("(^|;)\\s*" + key + "\\s*=\\s*([^;]+)");
 //     return b ? b.pop() : "";
@@ -40,58 +98,3 @@ import Link from "next/link";
 //         redirect("/");
 //     }
 // }
-async function checkUser(formData) {
-    const id = formData.get("id");
-    const password = formData.get("password");
-    const ref = collection(firebase, "users");
-    const querried = [];
-    const q = query(
-        ref,
-        where("idPatient", "==", id),
-        where("password", "==", password),
-    );
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-        querried.push(doc.data());
-    });
-
-    if (querried.length === 0) {
-        Cookies.set("logged_in", "");
-        // document.cookie = "logged_in=";
-        alert("User does not exist!");
-        console.log("User does not exist!");
-    } else {
-        // document.cookie = "logged_in=true";
-        Cookies.set("logged_in", "true");
-        redirect("/");
-    }
-}
-
-
-export default function LoginPage() {
-    // const idRef = useRef();
-    // const passRef = useRef();
-
-
-    // if (getCookie("logged_in")) {
-    if(Cookies.get("logged_in")) {
-        redirect("/");
-    }
-
-    return (
-        <div>
-            <form autoComplete="off" action={checkUser}>
-                <p>ID</p>
-                <input name="id" type="text" required/>
-                <p>Password</p>
-                <input name="password" type="password" required/>
-                <button>Login</button>
-                <h3>
-                    Do not have an account:
-                    <Link href="/pages/register">Register Here</Link>
-                </h3>
-            </form>
-
-        </div>
-    );
-}
