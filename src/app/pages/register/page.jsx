00779b0f327dc1585d48a9b57be7f2b1ff0794b1
Really_true_dude <andrekor2004@gmail.com>
2024-09-29 06:39:19 +0300

    log-reg

--- src/app/pages/register/page.jsx
+++ src/app/pages/register/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 import React, { useRef } from "react";
-import { redirect } from "next/navigation";
-import { firebase } from "../../../mvp-gh/src/app/utils/firebaseConfig";
+import {useRouter} from "next/navigation"
+// import { redirect } from "next/navigation";
+import { firebase } from "../../utils/firebaseConfig";
 import { addDoc, collection } from "@firebase/firestore";
+import Link from "next/link";
 
 export default function RegisterPage() {
+    const router = useRouter();
     // const [data, setData] = useState(null);
     // const [loading, setLoading] = useState(true);
     // const [error, setError] = useState(null);
@@ -34,34 +37,37 @@ export default function RegisterPage() {
 
         try {
             await addDoc(ref, data);
+            router.push("/pages/login");
         } catch (error) {
             console.log(error);
         }
-
-        redirect(`/login`);
     };
 
     return (
         <div>
             <h1>Register</h1>
             <div>
-                <form onSubmit={createUser}>
+                <form autoComplete="off" onSubmit={createUser}>
                     <p>First Name</p>
-                    <input className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500 focus:border-transparent" name="firstName" type="text" ref={firstNameRef} required />
+                    <input name="firstName" type="text" ref={firstNameRef} required/>
                     <p>Last Name</p>
-                    <input name="lastName" type="text" ref={lastNameRef} required />
+                    <input name="lastName" type="text" ref={lastNameRef} required/>
                     <p>Patronymic</p>
-                    <input name="patron" type="text" ref={patronRef} required />
+                    <input name="patron" type="text" ref={patronRef} required/>
                     <p>Phone Number</p>
-                    <input name="phone" type="text" ref={phoneRef} required />
+                    <input name="phone" type="text" ref={phoneRef} required/>
                     <p>Date of Birth</p>
-                    <input name="birthDate" type="text" ref={birthDateRef} required />
+                    <input name="birthDate" type="text" ref={birthDateRef} required/>
                     <p>ID</p>
-                    <input name="id" type="text" ref={idRef} required />
+                    <input name="id" type="text" ref={idRef} required/>
                     <p>Password</p>
-                    <input name="password" type="password" ref={passRef} required />
-                    <br />
+                    <input name="password" type="password" ref={passRef} required/>
+                    <br/>
                     <button type="submit">Register</button>
+                    <h3>
+                        Have an account:
+                        <Link href="/pages/login">Sign In</Link>
+                    </h3>
                 </form>
             </div>
         </div>
