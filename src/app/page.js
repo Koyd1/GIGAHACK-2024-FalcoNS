e6e45c292504fcf197536f58071d9d46552034d5
Alexandr Moroz <log.koyd@gmail.com>
2024-09-29 08:15:07 +0300

    final-ochka

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from "react";
 import Preloader from "@/app/components/Preloader/Preloader";
 import Challenges from "@/app/components/Challenges/Challenges";
 import Header from "@/app/components/Header/Header";
+import { redirect } from "next/navigation";
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-
+  redirect("/pages/home");
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
